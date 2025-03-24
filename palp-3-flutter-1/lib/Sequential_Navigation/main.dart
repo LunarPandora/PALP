0c46684a97dbb1f28bf162cbc13573c299e7963d
LunarPandora <wendycoolzzz74@gmail.com>
2025-03-24 16:29:19 +0700

    Removed debug banner.

--- palp-3-flutter-1/lib/Sequential_Navigation/main.dart
+++ palp-3-flutter-1/lib/Sequential_Navigation/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Sequential Navigation',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: OnboardingScreen(),
